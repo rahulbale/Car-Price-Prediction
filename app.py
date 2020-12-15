37480785cb683d0c40058571370b674fb0ef1380
rahulbale <71730686+rahulbale@users.noreply.github.com>
2020-12-16 00:41:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,13 +7,13 @@ import joblib
 
 app = Flask(__name__,template_folder="template",static_folder="static")
 
-model = joblib.load('cars_price_modal.pkl')
+#model = joblib.load('cars_price_modal.pkl')
 
 @app.route('/',methods=['GET'])
 def home():
     return render_template('index.html')
 
-
+"""
 standard_to = StandardScaler()
 @app.route("/predict", methods=['POST'])
 def predict():
@@ -38,6 +38,30 @@ def predict():
             return render_template('result.html',prediction_text="You Can Sell The Car at {}".format(output))
     else:
         return render_template('index.html')
+"""
 
-if __name__=="__main__":
+def ValuePredictor(to_predict_list, size):
+    to_predict = np.array(to_predict_list).reshape(1,size)
+    if(size==7):
+        loaded_model = joblib.load("cars_price_modal.pkl")
+        result = loaded_model.predict(to_predict)
+    return result[0]
+
+@app.route('/predict', methods = ["POST"])
+def predict():
+    if request.method == "POST":
+        to_predict_list = request.form.to_dict()
+        to_predict_list = list(to_predict_list.values())
+        to_predict_list = list(map(float, to_predict_list))
+         #diabetes
+        if(len(to_predict_list)==7):
+            result = ValuePredictor(to_predict_list,7)
+            if result<0:
+                return render_template('result.html',prediction_texts="Sorry you cannot sell this car")
+            else:
+                return render_template('result.html',prediction_text="You Can Sell The Car at {}".format(output))
+     else:
+          return render_template('index.html')  
+
+if __name__ == "__main__":
     app.run(debug=True)
