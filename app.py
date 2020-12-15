8e896b92452cfe2e0a422ef089e73b5cca5f0736
rahulbale <71730686+rahulbale@users.noreply.github.com>
2020-12-16 01:05:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,28 +7,26 @@ import joblib
 
 app = Flask(__name__,template_folder="template",static_folder="static")
 
-#model = joblib.load('cars_price_modal.pkl')
+model = joblib.load('cars_price_modal.pkl')
 
 @app.route('/',methods=['GET'])
 def home():
     return render_template('index.html')
 
-"""
+
 standard_to = StandardScaler()
 @app.route("/predict", methods=['POST'])
 def predict():
     if request.method == 'POST':
-        Year = int(request.form['Year'])
         Present_Price=float(request.form['Present_Price'])
         Kms_Driven=int(request.form['Kms_Driven'])
         Kms_Driven2=np.log(Kms_Driven)
         Fuel_Type=int(request.form['Fuel_Type_Petrol'])
-        Year=2020-Year
         Seller_Type=int(request.form['Seller_Type_Individual'])
         Transmission_Mannual=int(request.form['Transmission_Mannual'])
         Owner = int(request.form['Owner'])
 
-        prediction=model.predict([[Year,Present_Price,Kms_Driven2,Fuel_Type,Seller_Type,Transmission_Mannual,Owner]])
+        prediction=model.predict([[Present_Price,Kms_Driven,Fuel_Type,Seller_Type,Transmission_Mannual,Owner]])
 
         output=round(prediction[0],2)
 
@@ -38,30 +36,7 @@ def predict():
             return render_template('result.html',prediction_text="You Can Sell The Car at {}".format(output))
     else:
         return render_template('index.html')
-"""
 
-def ValuePredictor(to_predict_list, size):
-    to_predict = np.array(to_predict_list).reshape(1,size)
-    if(size==6):
-        loaded_model = joblib.load("cars_price_modal.pkl")
-        result = loaded_model.predict(to_predict)
-    return result[0]
-
-@app.route('/predict', methods = ["POST"])
-def predict():
-    if request.method == "POST":
-        to_predict_list = request.form.to_dict()
-        to_predict_list = list(to_predict_list.values())
-        to_predict_list = list(map(float, to_predict_list))
-         #diabetes
-        if(len(to_predict_list)==6):
-            result = ValuePredictor(to_predict_list,6)
-            if result<0:
-                return render_template('result.html',prediction_texts="Sorry you cannot sell this car")
-            else:
-                return render_template('result.html',prediction_text="You Can Sell The Car at {}".format(output))
-    else:
-        return render_template('index.html')  
 
 if __name__ == "__main__":
     app.run(debug=True)
