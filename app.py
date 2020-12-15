b4aa46fa5e6bc6596c2e13094e42a9afd1ff95d4
rahulbale <71730686+rahulbale@users.noreply.github.com>
2020-12-16 01:08:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,19 +14,21 @@ def home():
     return render_template('index.html')
 
 
-standard_to = StandardScaler()
-@app.route("/predict", methods=['GET','POST'])
+
+@app.route("/predict", methods=['POST'])
 def predict():
     if request.method == 'POST':
+        Year = int(request.form['Year'])
         Present_Price=float(request.form['Present_Price'])
         Kms_Driven=int(request.form['Kms_Driven'])
         Kms_Driven2=np.log(Kms_Driven)
         Fuel_Type=int(request.form['Fuel_Type_Petrol'])
+        Year=2020-Year
         Seller_Type=int(request.form['Seller_Type_Individual'])
         Transmission_Mannual=int(request.form['Transmission_Mannual'])
         Owner = int(request.form['Owner'])
 
-        prediction=model.predict([[Present_Price,Kms_Driven,Fuel_Type,Seller_Type,Transmission_Mannual,Owner]])
+        prediction=model.predict([[Year,Present_Price,Kms_Driven2,Fuel_Type,Seller_Type,Transmission_Mannual,Owner]])
 
         output=round(prediction[0],2)
 
@@ -36,7 +38,6 @@ def predict():
             return render_template('result.html',prediction_text="You Can Sell The Car at {}".format(output))
     else:
         return render_template('index.html')
-
 
 if __name__ == "__main__":
     app.run(debug=True)
