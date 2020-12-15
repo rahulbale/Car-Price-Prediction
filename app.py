d0e2f6f955fd023a64f4fd5141031dc4886ec6ef
rahulbale <71730686+rahulbale@users.noreply.github.com>
2020-12-16 01:05:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def home():
 
 
 standard_to = StandardScaler()
-@app.route("/predict", methods=['POST'])
+@app.route("/predict", methods=['GET','POST'])
 def predict():
     if request.method == 'POST':
         Present_Price=float(request.form['Present_Price'])
