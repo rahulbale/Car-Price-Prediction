17bf0f49ce4a74b287d37cfc8283488297d8dcbc
rahulbale <71730686+rahulbale@users.noreply.github.com>
2020-12-15 22:51:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from sklearn.preprocessing import StandardScaler
 
 app = Flask(__name__,template_folder="template",static_folder="static")
 
-model = pickle.load(open('../Car-Price-prediction/car_price_model.pkl', 'rb'))
+
 
 @app.route('/',methods=['GET'])
 def Home():
@@ -40,5 +40,5 @@ def predict():
         return render_template('index.html')
 
 if __name__=="__main__":
-    
+    model = pickle.load(open('car_price_model.pkl', 'rb'))
     app.run(debug=True)
