09dc799bea71fff05bc8933bfe729555dada2838
rahulbale <71730686+rahulbale@users.noreply.github.com>
2020-12-16 15:32:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def home():
     return render_template('index.html')
 
 
-"""
+
 @app.route("/predict", methods=['GET','POST'])
 def predict():
     if request.method == 'POST':
@@ -23,7 +23,7 @@ def predict():
         Present_Price=float(request.form['Present_Price'])
 
         Kms_Driven=int(request.form['Kms_Driven'])
-        Kms_Driven2=np.log(Kms_Driven)
+       
         Fuel_Type=int(request.form['Fuel_Type'])
 
         Year=2020-Year
@@ -34,11 +34,14 @@ def predict():
 
         Owner = int(request.form['Owner'])
 
-        prediction=model.predict([[Year,Present_Price,Kms_Driven2,Fuel_Type,Seller_Type,Transmission_Mannual,Owner]])
+        prediction=model.predict([[Year,Present_Price,Kms_Driven,Fuel_Type,Seller_Type,Transmission_Mannual,Owner]])
 
         output=round(prediction[0],2)
         
-        return render_template('result.html',prediction_texts="You Can Sell The Car at {}".format(output))
+        if output<0:
+            return render_template('result.html',prediction_texts="Sorry you cannot sell this car")
+        else:
+            return render_template('result.html',prediction_texts="You Can Sell The Car at {}".format(output))
     else:
         return render_template('index.html')
 
@@ -89,3 +92,4 @@ def predict():
 
 if __name__=="__main__":
     app.run(debug=True)
+    """
