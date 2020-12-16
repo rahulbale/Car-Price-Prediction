3b81db7b0e5c6530fd0b6d6345a65b8a4c4f626d
rahulbale <71730686+rahulbale@users.noreply.github.com>
2020-12-16 15:54:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request
-import pickle
 import pandas as pd
 import numpy as np
 import sklearn
@@ -14,41 +13,6 @@ def home():
     return render_template('index.html')
 
 
-
-@app.route("/predict", methods=['GET','POST'])
-def predict():
-    if request.method == 'POST':
-        Year = int(request.form['Year'])
-
-        Present_Price=float(request.form['Present_Price'])
-
-        Kms_Driven=int(request.form['Kms_Driven'])
-       
-        Fuel_Type=int(request.form['Fuel_Type'])
-
-        Year=2020-Year
-
-        Seller_Type=int(request.form['Seller_Type'])
-
-        Transmission_Mannual=int(request.form['Transmission'])
-
-        Owner = int(request.form['Owner'])
-
-        prediction=model.predict([[Year,Present_Price,Kms_Driven,Fuel_Type,Seller_Type,Transmission_Mannual,Owner]])
-
-        output=round(prediction[0],2)
-        
-        if output<0:
-            return render_template('result.html',prediction_texts="Sorry you cannot sell this car")
-        else:
-            return render_template('result.html',prediction_texts="You Can Sell The Car at {}".format(output))
-    else:
-        return render_template('index.html')
-
-if __name__=="__main__":
-    app.run(debug=True)
-   
-"""
 @app.route("/predict", methods=['POST'])
 def predict():
     Fuel_Type_Diesel=0
@@ -84,12 +48,12 @@ def predict():
         output=round(prediction[0],2)
         
         if output<0:
-            return render_template('result.html',prediction_texts="Sorry you cannot sell this car")
+            return render_template('index.html',prediction_texts="Sorry you cannot sell this car")
         else:
-            return render_template('result.html',prediction_texts="You Can Sell The Car at {}".format(output))
+            return render_template('index.html',prediction_texts="You Can Sell The Car at {}".format(output))
     else:
         return render_template('index.html')
 
 if __name__=="__main__":
     app.run(debug=True)
-    """
+
